26fa78f8eefac8236fe300fa3ec232d8eb815db2
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-02 17:33:05 +0000

    WebService Consultar pedido por idpedido

--- trunk/ItradeCobranza/WebServices/itrade/application/controllers/pedido.php
+++ trunk/ItradeCobranza/WebServices/itrade/application/controllers/pedido.php
@@ -26,5 +26,18 @@ class Pedido extends CI_Controller {
 			$this->output->set_content_type('application/json')->set_output(json_encode($result));					
 		}		
 	}
+	public function consultar_pedido($idpedido_w)
+	{
+		$idpedido=$this->input->post('idpedido');				
+		if (isset($idpedido_w)&& $idpedido_w!= "" ){			
+			$result=$this->Payment_model->get_by_id($idpedido_w);	
+			$this->output->set_content_type('application/json')->set_output(json_encode($result));			
+		}
+		
+		if (isset($idpedido)&& $idpedido!=""  ){
+			$result=$this->Payment_model->get_by_id($idpedido);
+			$this->output->set_content_type('application/json')->set_output(json_encode($result));					
+		}		
+	}
 }
 
