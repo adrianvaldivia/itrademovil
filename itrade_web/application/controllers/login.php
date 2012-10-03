c079a716b1223bc7a58f6c0fb73f0205e2d5d5a9
ly4ster <ly4ster@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-03 00:25:00 +0000

    agregando carpeta admin en controllers para los mantenimientos

--- itrade_web/application/controllers/login.php
+++ itrade_web/application/controllers/login.php
@@ -12,7 +12,7 @@ class Login extends CI_Controller {
         $data['title']="Itrade Mantenimientos!!!";             
         $data['main']="login/login_box.php";//RUTA			           			
         $this->load->vars($data);
-        $this->load->view('login/login');
+        $this->load->view('user_views/create_user_view');
         //echo "<script languaje='javascript'>alert('Index')</script>";
 	}
 }
