3d19015a856ea86705978631fd9f51bdaadef786
andrea.caceres@pucp.pe <andrea.caceres@pucp.pe@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-03 02:24:48 +0000

    WEB - USUARIO: MODELO Y CONTROLADOR - ANDREA , LUIS

---
+++ itrade_web/application/controllers/usuario_controller.php
@@ -0,0 +1,85 @@
+<?php
+class Usuario_controller extends CI_Controller {
+
+    function Usuario_controller(){
+        parent::__construct();
+        //Cargar los modelos para la base de datos
+        $this->load->model('Usuario_model');       
+		$this->load->model('Persona_model');     
+		
+		/*
+		$logged_in = $this->session->userdata('logged_in');
+
+	    if(!isset($logged_in) || $logged_in != TRUE){
+		$this->session->set_flashdata('message','Su sesión ha terminado.');
+		redirect('application/controllers/login','refresh');
+	    }
+	    if ($this->Rolemodel->has_access($this->session->userdata('role_id'), 'USER') == false ) {
+		$this->session->set_flashdata('warning','Usted no tiene acceso a este módulo.');
+		redirect('admin/dashboard');	
+	    }
+		*/
+    }
+	
+	public function index()
+	{
+        //cargar las noticias                
+        $data['title']="Itrade Mantenimientos!!!";             
+        $data['main']="login/login_box.php";//RUTA			           			
+        $this->load->vars($data);
+        $this->load->view('user_views/create_user_view');
+        //echo "<script languaje='javascript'>alert('Index')</script>";
+	}
+	public function set_validation_rules($rules)
+    {
+        if ($rules==0) //Rules para create
+        {
+            
+        }
+        if ($rules==1) //Rules para edit
+        {
+            $this->form_validation->set_rules('perfil', 'Perfil', 'required');
+            $this->form_validation->set_rules('activo', 'Activo', 'required');            
+        }
+        if ($rules==2 ) //Rules para info
+        {
+            
+        }
+        if ($rules==3) //Rules para Change
+        {
+            
+        }
+    }
+	
+	
+	
+	public function edit($idUsuario='')
+	{
+		//Parametro para setear reglas del edit
+        $rules_edit=1;
+        $this->set_validation_rules($rules_edit);
+
+		if ($this->form_validation->run()){
+			$data = array(
+			   'IdPerfil' => xss_clean($this->input->post('perfil')),
+			   'Activo' => xss_clean($this->input->post('activo')) )
+			);
+
+		    $this->usuario_model->edit($this->input->post('IdUsuario'), $data);
+		    $this->session->set_flashdata('message','Los datos del usuarios han sido modificados correctamente.');
+		    //redirect('admin/admin_seguridad/users','refresh'); FATLTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+		}else{
+    		    $data['error_message'] = validation_errors();
+		}
+		$data['title'] = "Editar Usuario";
+		$data['user'] = $this->usuario_model->get($idUsuario);		
+		//$data['main'] = 'admin/admin_seguridad/users/edit'; FALTAAAAAA
+		$this->load->vars($data);
+		$this->load->view('template');//LEILA REVISA ESTO
+	}
+	
+	
+	
+	
+}
+?>
--- itrade_web/application/models/usuario_model.php
+++ itrade_web/application/models/usuario_model.php
@@ -4,12 +4,26 @@ class Usuario_model extends CI_Model {
     function __construct()
     {        
         parent::__construct();
-		$this->tablename = 'usuario';	
+		$this->tablename = 'usuario';
     }
 	
 	function get_all_users(){		
 		$query = $this->db->get('usuario');		
         return $query->result();				
+	}
+	
+	function get($idUsuario) {
+		$this->db->select();
+		$this->db->where('IdUsuario', $idUsuario);		
+		$query = $this->db->get($this->tablename);		
+		$rows = $query->result();
+		return $rows[0];		
+	}
+	
+	function edit($idUsuario, $data)
+	{		
+		$this->db->where('IdUsuario', $idUsuario);	
+		return $this->db->update($this->tablename, $data);
 	}
 }
 ?>
