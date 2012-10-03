4c7ddf8f297e6db773be9fa1ec84eefef3d1faac
ly4ster <ly4ster@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-03 03:46:59 +0000

    modif  vista dashboard  by leila

---
+++ itrade_web/application/views/pages/dashboard_view.php
@@ -0,0 +1,7 @@
+<div id="contenido_principal" style="width:100%; height:100%;" >	
+    <div class="barra_lateral" style="width:30%; height:100%; min-width:350px; max-width:350px; float:left;">
+    </div>
+    <div class="contenido_principal" style="width:70%; height:100%;  min-width:650px; max-width:650px;">
+        <? echo anchor('admin/usuario_controller', 'Nuevo Usuario', array('title' => 'Nuevo Usuarios')); ?>
+    </div>
+</div>
