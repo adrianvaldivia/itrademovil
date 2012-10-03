158aef6238b61cd6b4c3e1c3bf542bbc5b423fc7
ly4ster <ly4ster@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-03 03:46:59 +0000

    modif  vista dashboard  by leila

--- trunk/itrade_web/application/views/pages/resultado.php
+++
@@ -1,6 +0,0 @@
-<div id="contenido_principal" style="width:100%; height:100%;" >	
-	<div class="barra_lateral" style="width:30%; height:100%; background-color:red; min-width:350px; max-width:350px; float:left;">
-	</div>
-	<div class="contenido_principal" style="width:70%; height:100%; background-color:blue; min-width:650px; max-width:650px; float:left;">
-	</div>
-</div>
