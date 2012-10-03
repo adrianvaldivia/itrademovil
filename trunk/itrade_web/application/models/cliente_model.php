3a5a175cf4fbaeaa6fd0600d6aecb3df614fbd92
andrea.caceres@pucp.pe <andrea.caceres@pucp.pe@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-03 03:45:10 +0000

    WEB - FUNCIONES aMODELOS

--- trunk/itrade_web/application/models/cliente_model.php
+++ trunk/itrade_web/application/models/cliente_model.php
@@ -11,5 +11,36 @@ class Cliente_model extends CI_Model {
 		$query = $this->db->get('cliente');		
         return $query->result();				
 	}
+
+	function get($idCliente) {
+        $this->db->where('IdCliente', $idCliente);
+        $query = $this->db->get($this->tablename);
+        $rows = $query->result();
+        return $rows[0];
+    }
+
+    function edit($idCliente, $data) {
+        $this->db->where('IdCliente', $idCliente);
+        return $this->db->update($this->tablename, $data);
+    }
+
+    function get_by_idPersona($idPersona) {
+        $this->db->where('IdPersona', $idPersona);
+        $query = $this->db->get($this->tablename);
+        return $query->row_array();
+    }
+
+	function get_by_razonSocial($razonSocial) {
+        $this->db->where('RazonSocial', $razonSocial);
+        $query = $this->db->get($this->tablename);
+        return $query->row_array();
+    }
+
+	function get_by_ruc($ruc) {
+        $this->db->where('RUC', $ruc);
+        $query = $this->db->get($this->tablename);
+        return $query->row_array();
+    }
+    
 }
 ?>
--- trunk/itrade_web/application/models/jerarquia_model.php
+++ trunk/itrade_web/application/models/jerarquia_model.php
@@ -11,6 +11,24 @@ class Jerarquia_model extends CI_Model {
 		$query = $this->db->get('jerarquia');		
         return $query->result();				
 	}
+
+    function get($idJerarquia) {
+        $this->db->where('IdJerarquia', $idJerarquia);
+        $query = $this->db->get($this->tablename);
+        $rows = $query->result();
+        return $rows[0];
+    }
+
+    function edit($idJerarquia, $data) {
+        $this->db->where('IdJerarquia', $idJerarquia);
+        return $this->db->update($this->tablename, $data);
+    }
+
+    function get_by_description($description) {
+        $this->db->where('Descripcion', $description);
+        $query = $this->db->get($this->tablename);
+        return $query->row_array();
+    }
 }
 
 ?>
