11fd09c9b5238842a26e683e2e191f87f61c9522
ly4ster <ly4ster@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-03 02:56:44 +0000

    modif adminstraacion usuarios by leila

--- itrade_web/application/models/usuario_model.php
+++ itrade_web/application/models/usuario_model.php
@@ -1,29 +1,46 @@
 <?
+
 class Usuario_model extends CI_Model {
 
-    function __construct()
-    {        
+    function __construct() {
         parent::__construct();
-		$this->tablename = 'usuario';
+        $this->tablename = 'usuario';
     }
-	
-	function get_all_users(){		
-		$query = $this->db->get('usuario');		
-        return $query->result();				
-	}
-	
-	function get($idUsuario) {
-		$this->db->select();
-		$this->db->where('IdUsuario', $idUsuario);		
-		$query = $this->db->get($this->tablename);		
-		$rows = $query->result();
-		return $rows[0];		
-	}
-	
-	function edit($idUsuario, $data)
-	{		
-		$this->db->where('IdUsuario', $idUsuario);	
-		return $this->db->update($this->tablename, $data);
-	}
+
+    function get_all_users() {
+        $query = $this->db->get($this->tablename);
+        return $query->result();
+    }
+
+    function get($idUsuario) {
+        $this->db->where('IdUsuario', $idUsuario);
+        $query = $this->db->get($this->tablename);
+        $rows = $query->result();
+        return $rows[0];
+    }
+
+    function edit($idUsuario, $data) {
+        $this->db->where('IdUsuario', $idUsuario);
+        return $this->db->update($this->tablename, $data);
+    }
+
+    function get_by_username($username) {
+        $this->db->where('IdUsuario', $username);
+        $query = $this->db->get($this->tablename);
+        return $query->row_array();
+    }
+
+    public function verify_user($username, $password) {
+        $this->db->where('IdUsuario', $username);
+        $this->db->where('Password', do_hash(xss_clean($password)));
+        $query = $this->db->get($this->tablename);
+
+        if ($query->num_rows != 1) {
+            return FALSE;
+        }
+        return TRUE;
+    }
+
 }
+
 ?>
