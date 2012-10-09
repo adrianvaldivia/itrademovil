e8d39875bab27eaa797b555e7ebc42221a3014bc
ly4ster <ly4ster@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-09 00:24:22 +0000

    modif modelos   by leila

--- itrade_web/application/models/persona_model.php
+++ itrade_web/application/models/persona_model.php
@@ -1,31 +1,43 @@
 <?
+
 class Persona_model extends CI_Model {
 
-    function __construct()
-    {        
+    function __construct() {
         parent::__construct();
-		$this->tablename = 'persona';	
+        $this->tablename = 'Persona';
+        $this->table_usuario = 'Usuario';
     }
-	
-	function get_all_people(){		
+
+    function get_all_people() {
         $query = $this->db->get($this->tablename);
-        return $query->result();				
-	}
-	
+        return $query->result_array();
+    }
+
     function get($idPersona) {
         $this->db->where('IdPersona', $idPersona);
         $query = $this->db->get($this->tablename);
         $rows = $query->result();
         return $rows[0];
     }
-	
-	function create($data)
-	{
-		$this->db->insert($this->tablename, $data);
-		$query = $this->db->query('select last_insert_id()');
-		$row = $query->row_array();
-		return $row['last_insert_id()'];
-	}
+
+    function get_last_idPersona() {
+        $this->db->select_max('IdPersona');
+        $query = $this->db->get($this->tablename);
+        return $query->row(0)->IdPersona;
+    }
+
+    function get_idPersona($idusuario) {
+        $this->db->select($this->table_usuario . ".IdPersona");
+        $this->db->where($this->tablename . '.IdPersona', $idusuario);
+        $this->db->from($this->tablename);
+        $this->db->join($this->table_usuario, $this->table_usuario . '.IdPersona=' . $this->tablename . '.IdPersona');
+        $query = $this->db->get();
+        return $query->row(0)->IdPersona;
+    }
+
+    function create($data) {
+        return $this->db->insert($this->tablename, $data);
+    }
 
     function edit($idPersona, $data) {
         $this->db->where('IdPersona', $idPersona);
@@ -37,24 +49,25 @@ class Persona_model extends CI_Model {
         $query = $this->db->get($this->tablename);
         return $query->row_array();
     }
-	
-	function get_by_firstLastName($firstLastName) {
+
+    function get_by_firstLastName($firstLastName) {
         $this->db->where('ApePaterno', $firstLastName);
         $query = $this->db->get($this->tablename);
         return $query->row_array();
     }
-	
-	function get_by_secondLastName($secondLastName) {
+
+    function get_by_secondLastName($secondLastName) {
         $this->db->where('ApeMaterno', $secondLastName);
         $query = $this->db->get($this->tablename);
         return $query->row_array();
-    }    
+    }
 
-	function get_by_dni($dni) {
+    function get_by_dni($dni) {
         $this->db->where('DNI', $dni);
         $query = $this->db->get($this->tablename);
         return $query->row_array();
     }
-    
+
 }
+
 ?>
