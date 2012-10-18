c9fb428b38cf15203c26253c5ffc3700e5d62609
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-18 17:34:37 +0000

    Integrado webservices

--- itrade_web/application/controllers/ws/clientes.php
+++ itrade_web/application/controllers/ws/clientes.php
@@ -5,7 +5,7 @@ class Clientes extends CI_Controller {
 	function __construct()
     {        
         parent::__construct();
-		$this->load->model('Cliente_model');
+		$this->load->model('ws/Cliente_model');
     }	
 
 	public function index()
