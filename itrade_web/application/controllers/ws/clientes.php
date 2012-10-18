063fbe888c8188f2e90afb0edecf0c3391a80ce8
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-18 16:06:26 +0000

    WebService Clientes

---
+++ itrade_web/application/controllers/ws/clientes.php
@@ -0,0 +1,42 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Clientes extends CI_Controller {
+	
+	function __construct()
+    {        
+        parent::__construct();
+		$this->load->model('Cliente_model');
+    }	
+
+	public function index()
+	{					
+		echo "Controlador Clientes";			
+	}
+	
+	public function get_clientes_by_vendedor($idvendedor_w='')
+	{
+		$idvendedor=$this->input->post('idvendedor');				
+		if (isset($idvendedor_w)&& $idvendedor_w!= "" ){			
+			$result=$this->Cliente_model->get_clients_by_idvendedor($idvendedor_w);	
+			$this->output->set_content_type('application/json')->set_output(json_encode($result));		
+		}		
+		if (isset($idvendedor)&& $idvendedor!=""  ){
+			$result=$this->Cliente_model->get_clients_by_idvendedor($idvendedor);	
+			$this->output->set_content_type('application/json')->set_output(json_encode($result));					
+		}		
+	}	
+	
+	public function get_pedidos_by_idvendedor($idvendedor_w='')
+	{
+		$idvendedor=$this->input->post('idvendedor');				
+		if (isset($idvendedor_w)&& $idvendedor_w!= "" ){			
+			$result=$this->Cliente_model->get_pedidos_by_idvendedor($idvendedor_w);	
+			$this->output->set_content_type('application/json')->set_output(json_encode($result));		
+		}		
+		if (isset($idvendedor)&& $idvendedor!=""  ){
+			$result=$this->Cliente_model->get_pedidos_by_idvendedor($idvendedor);	
+			$this->output->set_content_type('application/json')->set_output(json_encode($result));					
+		}		
+	}
+}
+
