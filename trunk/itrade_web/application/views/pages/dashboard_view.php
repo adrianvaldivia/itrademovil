f6adac80fecfaca88ff15aaaeb6e63a2b3897e6a
ly4ster <ly4ster@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-21 18:21:41 +0000

    todo menos validaciones x_x   by leila

--- trunk/itrade_web/application/views/pages/dashboard_view.php
+++ trunk/itrade_web/application/views/pages/dashboard_view.php
@@ -1,13 +1,18 @@
 
 
-    <div class="barra_lateral" style="width:30%; height:100%; min-width:350px; max-width:350px; float:left;">
-	 <? if($acceso==1) {
-			echo anchor('admin/usuario_controller/', 'Administrar Usuarios', array('title' => 'Administrar Usuarios')); 
-			echo "<br />";
-			
-			echo anchor('admin/credito_controller/', 'Administrar Credito', array('title' => 'Administrar Credito')); 
-			echo "<br />";
-			
-         }
-	?>
-    </div>
+<div class="barra_lateral" style="width:30%; height:100%; min-width:350px; max-width:350px; float:left;">
+    <?
+    if ($acceso == 1) {
+        echo anchor('admin/usuario_controller/', 'Administrar Usuarios', array('title' => 'Administrar Usuarios'));
+        echo "<br />";
+
+//        echo anchor('admin/credito_controller/', 'Administrar Credito', array('title' => 'Administrar Credito'));
+//        echo "<br />";
+    }
+    if ($acceso == 4) {
+ 
+        echo anchor('admin/credito_controller/', 'Administrar Credito', array('title' => 'Administrar Credito'));
+        echo "<br />";
+    }
+    ?>
+</div>
