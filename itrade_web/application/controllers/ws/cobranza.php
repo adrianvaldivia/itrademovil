87b5d7edc9de9969eee826704443158d06c7d428
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-22 05:02:12 +0000

    get all productos

---
+++ itrade_web/application/controllers/ws/cobranza.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Cobranza extends CI_Controller {
+	
+	function __construct()
+    {        
+        parent::__construct();
+		$this->load->model('ws/Product_model');
+    }	
+
+	public function index()
+	{					
+		echo "Controlador Pedido";			
+	}	
+	public function get_all_products()
+	{				
+		$result=$this->Product_model->get_all_products();	
+		$this->output->set_content_type('application/json')->set_output(json_encode($result));	
+	}
+
+}
+
