e884aa3c3bef52a43670706ff02eb18107034fd7
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-22 05:02:37 +0000

    get all products

---
+++ trunk/itrade_web/application/models/ws/product_model.php
@@ -0,0 +1,18 @@
+<?
+class Product_model extends CI_Model {
+
+    function __construct()
+    {        
+        parent::__construct();		
+		$this->table_producto = 'Producto';		
+    }	
+	
+	public function get_all_products(){		
+		$this->db->from($this->table_producto);					
+		$this->db->where($this->table_producto.".Activo", 1);			
+		$query = $this->db->get();
+		return $query->result();			
+	}
+		
+}
+?>
