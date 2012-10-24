362ee90f2643d992affca96918c97d1c817e50a4
ly4ster <ly4ster@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-24 13:14:50 +0000

    vistas x_x   by leila

--- trunk/itrade_web/application/views/login/login.php
+++ trunk/itrade_web/application/views/login/login.php
@@ -4,79 +4,93 @@
     <head>
         <!-- Meta -->
         <meta http-equiv="Content-type" content="text/html;charset=UTF-8"> 
-        <!-- End of Meta -->
+            <!-- End of Meta -->
 
-        <title><?php echo $title; ?></title>   
+            <title><?php echo $title; ?></title>   
 
-        <!--styleshets CSS-->
-        <link href="<?php echo base_url() ?>css/login.css" rel="stylesheet" />	
-        <link href="<?php echo base_url() ?>css/login_box.css" rel="stylesheet" />	
+            <!--styleshets CSS-->
+    <!--        <link href="<?php echo base_url() ?>css/tripoli.simple.css" rel="stylesheet" />
+            <link href="<?php echo base_url() ?>css/layout.css" rel="stylesheet" />		-->
+            <link href="<?php echo base_url() ?>css/login_box.css" rel="stylesheet" />
+             <link href="<?php echo base_url() ?>css/login.css" rel="stylesheet" />	
 
-        <!--scripts js-->
-        <script type="text/javascript" src="<?php echo base_url() ?>js/jquery-1.2.3.pack.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>js/jquery-1.7.2.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>js/jquery-1.7.2.min.js"></script>
+            <!--scripts js-->
+            <script type="text/javascript" src="<?php echo base_url() ?>js/jquery-1.2.3.pack.js"></script>
+            <script type="text/javascript" src="<?php echo base_url() ?>js/jquery-1.7.2.js"></script>
+            <script type="text/javascript" src="<?php echo base_url() ?>js/jquery-1.7.2.min.js"></script>
 
-        <script type="text/javascript">
-            //<![CDATA[
-            base_url = '<?php echo base_url(); ?>';
-            //]]>
-        </script>
-		
-		<!--[if IE]>
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/ie.css" media="screen, projection, print" />
-	<![endif]-->
-	 <!--[if lt IE 7]>
-	<script src="js/DD_belatedPNG_0.0.7a-min.js" type="text/javascript"></script>
-	<script>
+            <script type="text/javascript">
+                //<![CDATA[
+                base_url = '<?php echo base_url(); ?>';
+                //]]>
+            </script>
 
-		DD_belatedPNG.fix(' #header, h1, h1 a, .close, .field,.paginate .current, .icon, .required-icon');
+            <!--[if IE]>
+            <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/ie.css" media="screen, projection, print" />
+    <![endif]-->
+            <!--[if lt IE 7]>
+           <script src="js/DD_belatedPNG_0.0.7a-min.js" type="text/javascript"></script>
+           <script>
+   
+                   DD_belatedPNG.fix(' #header, h1, h1 a, .close, .field,.paginate .current, .icon, .required-icon');
+   
+           </script>
+             /* <link rel="stylesheet" href="<?php echo base_url() ?>css/ie6.css" type="text/css" media="screen, projection"/> */
+           <![endif]-->
 
-	</script>
-	  /* <link rel="stylesheet" href="<?php echo base_url() ?>css/ie6.css" type="text/css" media="screen, projection"/> */
-	<![endif]-->
-	
     </head>
     <body>
+      <!--  <div id="header">
+            <div class="container content">
+                <h1><a href="<? echo base_url() ?>home" title="Back to home">i-Trade</a></h1>
 
-        <div id="header">
-            <!--            <div id="title">iTRADE</div>-->
-            <div id="logo">
-                <a href="<?echo base_url()?>home"> <img src="<?php echo base_url() ?>images/loguito.png"/></a>
-            </div>
-            <? if ($this->session->userdata('logged_in') && (isset($username) && isset($name))) { ?>
-                <div class="header_left"  >
-                    <span class="bienvenido">Bienvenido <span class="user"><? echo trim($username); ?></span></span>
-                    <span class="nombre"><? echo trim($name); ?>
-                    </span>
-                </div>
-                <div class="header_right" >
-                    <a href="<?echo base_url()?>home/logout"><img src="<? echo base_url() ?>images/logout.png" /></a>
-                    <? echo anchor(base_url().'home/logout', 'Logout', array('title' => 'Salir de la sesion')); ?>
-                </div>
-    <!--                <script type="text/javascript">
-                    $('#header .header_right').click(function (){
-                        $(this).children('a').each(function(){
-                           $(this).click();
-                        });
-                    });
-                </script> -->
-            <? } else { ?>
-                <? //si entra un usuario logueado al home ?>
-            <? } ?>
-        </div>
-        <div id="content">
-            <div id="contenido_principal" style="width:100%; height:100%;" >
-
-                <?php
-//print($main);
-                 $this->load->view($main); 
-                ?>
+                <? if ($this->session->userdata('logged_in') && (isset($username) && isset($name))) { ?>
+                    <div class="loginInfos"> You're logged in as <strong><? echo trim($username); ?></strong>.  <? echo anchor(base_url() . 'home/logout', 'Logout', array('title' => 'Salir de la sesion')); ?> </div>
+                <? } else { ?>
+                    <? //si entra un usuario logueado al home ?>
+                <? } ?>
 
             </div>
-        </div>
-<!--        <div id="footer">
-            <div>2012 - i-Trade</div>
         </div>-->
+
+                <div id="header">
+                             <!--   <div id="title">iTRADE</div>-->
+                    <div id="logo">
+                        <a href="<? echo base_url() ?>home"> <img src="<?php echo base_url() ?>images/logo.png"/></a>
+                    </div>
+        <? if ($this->session->userdata('logged_in') && (isset($username) && isset($name))) { ?>
+                            <div class="header_left"  >
+                                <span class="bienvenido">Bienvenido <span class="user"><? echo trim($username); ?></span></span>
+                                <span class="nombre"><? echo trim($name); ?>
+                                </span>
+                            </div>
+                            <div class="header_right" >
+                                <a href="<? echo base_url() ?>home/logout"><img src="<? echo base_url() ?>images/logout.png" /></a>
+            <? echo anchor(base_url() . 'home/logout', 'Logout', array('title' => 'Salir de la sesion')); ?>
+                            </div>
+                                <script type="text/javascript">
+                                $('#header .header_right').click(function (){
+                                    $(this).children('a').each(function(){
+                                       $(this).click();
+                                    });
+                                });
+                            </script> 
+        <? } else { ?>
+            <? //si entra un usuario logueado al home ?>
+        <? } ?>
+                </div>
+                <div id="content">
+                   <div id="contenido_principal" style="width:100%; height:100%;" >
+
+        <?php
+//print($main);
+        $this->load->view($main);
+        ?>
+
+        <!--            </div>-->
+        <!--        </div>-->
+        <!--        <div id="footer">
+                    <div>2012 - i-Trade</div>
+                </div>-->
     </body>
 </html>
