ce777a3bad26d3b3e730947ed7d3a12b23d7a7c5
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-24 21:03:23 +0000

    Web service registro prospecto

--- itrade_web/application/models/ws/cliente_model.php
+++ itrade_web/application/models/ws/cliente_model.php
@@ -8,6 +8,7 @@ class Cliente_model extends CI_Model {
 		$this->table_persona = 'Persona';
 		$this->table_pedido = 'Pedido';
 		$this->table_estado_pedido = 'EstadoPedido';
+		$this->table_linea = 'Linea_Credito';
     }	
 	
 	public function get_clients_by_idvendedor($idvendedor){				
@@ -108,5 +109,74 @@ class Cliente_model extends CI_Model {
 		//echo $this->db->last_query();
 		return $query->result();	
 	}
+	public function registrerProspecto($ruc,$razon_social,$direccion,$nombre,$apepaterno,$apematerno,$telefono,$fechanac,$dni,$montosolicitado,$idvendedor){		
+		$this->db->flush_cache();
+		$idpersona=0;
+		$this->registrarPersona($nombre, $apepaterno, $apematerno,$telefono,$fechanac,$dni);
+		//echo "persona".$idpersona;
+		$idpersona=$this->get_last_idPersona();
+		//print_r($idpersona);
+		if ($idpersona!=0){
+			$this->registrarProspecto($idpersona,$ruc,$razon_social,$direccion,$idvendedor);
+			$idcliente=$this->get_last_idCliente();	
+			if ($idcliente!=0){
+				$this->registrarLineaCredito($idcliente,$montosolicitado);
+				$idlinea=$this->get_last_idlinea();
+				if ($idlinea!=0){
+					return "true";
+				}else{
+					return "false";
+				}
+			}else{
+				return "false";
+			}
+		}else{
+			return "false";
+		}
+			
+	}
+	public function registrarLineaCredito($idcliente,$montosolicitado){
+		$this->db->flush_cache();
+		$idlinea=$this->get_last_idlinea();
+		$data=array("IdLinea"=>$idlinea+1,"MontoSolicitado"=>$montosolicitado,"IdCliente"=>$idcliente,"Activo"=>1);	
+		$this->db->insert($this->table_linea, $data);
+		//return $this->db->insert_id($this->table_linea, $data);
+	}
+	
+	public function registrarProspecto($idpersona,$ruc,$razon_social,$direccion,$idvendedor){
+		$this->db->flush_cache();
+		$idcliente=$this->get_last_idCliente();
+		$data=array("IdCliente"=>$idcliente+1,"IdPersona"=>$idpersona,"Razon_Social"=>$razon_social,"RUC"=>$ruc,"Direccion"=>$direccion,"IdVendedor"=>$idvendedor,"IdEstado"=>1);	
+		$this->db->insert($this->table_cliente, $data);
+		//return $this->db->insert_id($this->table_cliente, $data);
+	}
+	
+	
+	public function registrarPersona($nombre, $apepaterno, $apematerno,$telefono,$fechanac,$dni){
+		$this->db->flush_cache();
+		$idpersona=$this->get_last_idPersona();
+		$data=array("IdPersona"=>$idpersona+1,"Nombre"=>$nombre,"ApePaterno"=>$apepaterno,"ApeMaterno"=>$apematerno,"Telefono"=>$telefono,"FechNac"=>$fechanac,"DNI"=>$dni);
+		$this->db->insert($this->table_persona, $data);		
+		//return $this->db->insert_id($this->table_persona, $data);
+	}
+	
+	public function get_last_idPersona(){
+		$this->db->flush_cache();
+        $this->db->select_max('IdPersona');
+        $query = $this->db->get($this->table_persona);
+        return $query->row(0)->IdPersona;
+    }
+	public function get_last_idCliente(){
+		$this->db->flush_cache();
+        $this->db->select_max('IdCliente');
+        $query = $this->db->get($this->table_cliente);
+        return $query->row(0)->IdCliente;
+    }
+	public function get_last_idlinea(){
+		$this->db->flush_cache();
+        $this->db->select_max('IdLinea');
+        $query = $this->db->get($this->table_linea);
+        return $query->row(0)->IdLinea;
+    }
 }
 ?>
