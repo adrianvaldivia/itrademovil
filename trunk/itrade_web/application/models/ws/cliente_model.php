c5f5357c67ff7e98102821e595343f149a4a71aa
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-24 21:13:33 +0000

    get by idvendedor

--- trunk/itrade_web/application/models/ws/cliente_model.php
+++ trunk/itrade_web/application/models/ws/cliente_model.php
@@ -40,6 +40,35 @@ class Cliente_model extends CI_Model {
 		//echo $this->db->last_query();
 		return $query->result();	
 	}
+	public function get_clients_by_idvendedor_p($idvendedor){				
+		//IDPEDIDO, IDCLIENTE, NOMBRECLIENTE, MONTOTOTAL				
+		$this->db->select($this->table_persona.".IdPersona, ".
+						$this->table_cliente.".IdCliente, ".
+						$this->table_persona.".Nombre, ".
+						$this->table_persona.".ApePaterno, ".
+						$this->table_cliente.".Razon_Social, ".
+						$this->table_cliente.".RUC, ".
+						$this->table_cliente.".Latitud, ".
+						$this->table_cliente.".Longitud, ".
+						$this->table_cliente.".Direccion, ".
+						$this->table_cliente.".IdCobrador, ".
+						$this->table_persona.".ApeMaterno ");
+						
+		$this->db->from($this->table_pedido);
+		$this->db->join($this->table_cliente,$this->table_pedido.".IdCliente =".$this->table_cliente.".IdCliente");				
+		$this->db->join($this->table_persona,$this->table_persona.".IdPersona =".$this->table_cliente.".IdPersona");				
+		//Restriccion del día de hoy				
+		$dates="(DATEDIFF( CURDATE(), ".$this->table_pedido.".FechaPedido)=7)";
+		$this->db->where($dates);		
+		//Restriccion de un solo vendedor
+		$this->db->where($this->table_cliente.".IdVendedor", $idvendedor);					
+		//Restriccion de pedidos en estado pendiente
+		$this->db->where($this->table_pedido.".IdEstadoPedido", 1);		
+		$this->db->group_by($this->table_cliente.".IdCliente");	
+		$query = $this->db->get();
+		//echo $this->db->last_query();
+		return $query->result();	
+	}
 	public function get_pedidos_by_idvendedor($idvendedor){
 		$this->db->select($this->table_pedido.".IdCliente, ".
 							$this->table_pedido.".IdPedido, ".
