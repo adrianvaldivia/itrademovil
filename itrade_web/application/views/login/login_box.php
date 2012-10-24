168791ca64f21230f66d6a7b60667e6e6c28f40c
ly4ster <ly4ster@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-10-24 13:14:50 +0000

    vistas x_x   by leila

--- itrade_web/application/views/login/login_box.php
+++ itrade_web/application/views/login/login_box.php
@@ -1,34 +1,112 @@
-<!-- Content -->
+<div id="login_principal">
+    <fieldset >
+        <legend><b>INTRANET</b></legend>   
+        <?php if ($this->session->flashdata('error')) { ?>
+            <div class='message' style="text-align:center; margin-bottom:10px; color:red">
+                <?php echo $this->session->flashdata('error') ?>
+            </div>
+        <?php } ?>		
+        <?php echo form_open("validate/validate_admin", array('id' => 'loginForm', 'name' => 'loginForm')); ?>            
+        <table >
+            <tr>
+                <td style="width: 45%;text-align:right;padding:5px;color:darkgray;"><label><b>Usuario: </b></label></td>
+                <td style="width: 55%">
+                    <?php $udata = array('name' => 'username', 'id' => 'username'); ?>
+                    <?php echo form_input($udata) ?>						
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align:right;padding:5px;color: darkgray"><label><b>Contrase&ntilde;a: </b></label></td>
+                <td>
+                    <?php $pdata = array('name' => 'password', 'id' => 'password'); ?>
+                    <?php echo form_password($pdata) ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td style="text-align:right;padding:5px;"> <!--<label>Combobox</label>--> </td>
+                <td>
+                    <?
+                    $options = array(
+                        'small' => 'Small Shirt',
+                        'med' => 'Medium Shirt',
+                        'large' => 'Large Shirt',
+                        'xlarge' => 'Extra Large Shirt',
+                    );
+                    $shirts_on_sale = array('small', 'med');
+                    /* echo form_dropdown('shirts', $options, ''); */ //Nombre, opciones del combo, seleccionado
+                    ?>
+                </td>
+
+            </tr>
+
+            <tr>
+                <td style="text-align:right;padding:5px;"> <!--<label>CheckBOX: </label>--> </td>
+                <td>
+                    <?php /* echo form_checkbox('check', 'accept', FALSE); */ ?>											
+                </td>
+            </tr>	
+            <tr>
+                <td style="text-align:right;padding:5px;"> <!--<label>RADIOBUTTON: </label>--> </td>
+                <td>
+                    <?php /* echo form_radio('radio', 'MACHO', FALSE); */ ?>											
+                    <?php /* echo form_radio('radio', 'HEMBRA', FALSE); */ ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td>&nbsp;</td>
+                <td style="padding:5px;">
+                    <?
+                    $data = array(
+                        'name' => 'button_submit',
+                        'id' => 'button_submit',
+                        'type' => 'submit',
+                        'content' => 'Ingresar'
+                    );
+                    echo form_button($data);
+                    ?>
+                </td>
+            </tr>
+        </table>
+        <input type="hidden" value="2" name="user_type">
+        <?php echo form_close(); ?>
+    </fieldset>
+
+</div>
+
+
+<!-- Content 
 	<div id="login" class="content">	
-		<div class="roundedBorders login-box">
-			<!-- Title -->
+		<div class="roundedBorders login-box">-->
+			<!-- Title
 			<div id="title" class="b2">
-				<h2>Log In</h2>
-				<!-- TitleActions -->
+				<h2>Log In</h2> -->
+				<!-- TitleActions 
 				<div id="titleActions">
 					<div class="actionBlock">
 					<a href="#">Forgot your password ?</a>
 					</div>
-				</div>
-				<!-- /TitleActions -->
-			</div>
+				</div>-->
+				<!-- /TitleActions 
+			</div>-->
 			<!-- Title -->
 	
-			<!-- Inner Content -->
+			<!-- Inner Content
 			<div id="innerContent">
-				<!-- <form action="titanium.php" method="post"> -->
+				
 				 <?php echo form_open("validate/validate_admin", array('id' => 'loginForm', 'name' => 'loginForm')); ?> 
 					<div class="field">
 						<label for="username">Usuario: </label>
-						<?php $udata = array('name' => 'username', 'id' => 'username'); ?>
+						<?php $udata = array('name' => 'username', 'id' => 'username','class'=>'text'); ?>
                     <?php echo form_input($udata) ?>
-						<!-- <input type="text" class="text" id="username" name="username" value="just click login !"/> -->
+						
 					</div>
 					<div class="field">
 									
 						<label for="password">Contraseña: </label>
-						<!-- <input type="password" class="text" id="password" name="password" value="aaaaaa"/> -->
-						<?php $pdata = array('name' => 'password', 'id' => 'password'); ?>
+						
+						<?php $pdata = array('name' => 'password', 'id' => 'password','class'=>'text'); ?>
                     <?php echo form_password($pdata) ?>
 					</div>
 					<div class="clearfix login-submit">
@@ -42,18 +120,19 @@
                         'name' => 'button_submit',
                         'id' => 'button_submit',
                         'type' => 'submit',
-                        'content' => 'Ingresar'
+                        'content' => '<strong>Ingresar</strong>',
+                        'class'=>'button'
                     );
                     echo form_button($data);
                     ?>
-							<!-- <button class="button" type="submit"><strong>Log In</strong></button> -->
+							
 						</span>
-					</div>
+					</div> -->
 				<!-- </form> -->
-				 <input type="hidden" value="2" name="user_type">
+			<!-- 	 <input type="hidden" value="2" name="user_type">-->
         <?php echo form_close(); ?>
-			</div>
-			<!-- /Inner Content -->
+			<!--</div>-->
+			<!-- /Inner Content
 			<div class="bBottom"><div></div></div>
 		</div>
-	</div>
+	</div>-->
