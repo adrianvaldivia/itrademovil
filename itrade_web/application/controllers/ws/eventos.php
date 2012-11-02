edac16f6a32046dd5a6e839c5363a4844f3a5e1d
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-11-02 00:46:45 +0000

    pedidos and evento

---
+++ itrade_web/application/controllers/ws/eventos.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Eventos extends CI_Controller {
+	
+	function __construct()
+    {        
+        parent::__construct();		
+		$this->load->model('ws/Evento_model');
+
+    }	
+
+	public function index()
+	{					
+		echo "Controlador Pedido";			
+	}	
+	
+	public function registrar_evento($idcreador_w='',$descripcion_w='',$fecha_w='',$horaini_w='',$horafin_w=''){
+		
+		$idcreador=$this->input->post('idcreador');
+		$descripcion=$this->input->post('descripcion');
+		$fecha=$this->input->post('fecha');
+		$horaini=$this->input->post('horaini');
+		$horafin=$this->input->post('horafin');				
+		if (isset($idcreador_w)&& $idcreador_w!=""  ){
+			$idcreador=$idcreador_w;
+			$descripcion=$descripcion_w;
+			$fecha=$fecha_w;
+			$horaini=$horaini_w;
+			$horafin=$horafin_w;
+		}
+		$result=$this->Evento_model->registrar_evento($idcreador,$descripcion,$fecha,$horaini,$horafin);
+		$this->output->set_content_type('application/json')->set_output(json_encode($result));					
+	}
+}
+
