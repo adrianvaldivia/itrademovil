edd214c8237184895cbaa4c2a08d7a3492d5d996
jorgecornejo0487@gmail.com <jorgecornejo0487@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-11-14 20:58:28 +0000

    jc

--- trunk/ITradeMovil/src/com/itrade/db/DAOProspecto.java
+++ trunk/ITradeMovil/src/com/itrade/db/DAOProspecto.java
@@ -69,6 +69,52 @@ private Activity window;
 		
 	}
 	
-	
+
+public void registrarProspecto(String idusuario, String dni, String nombre, String paterno, String materno, String fono,
+									String correo, String fecha, String ruc, String rsocial, String direcc, String latitud,
+									String longitud) {
+		
+		Intent i = this.window.getIntent();                
+		//idusuario=String.valueOf(idUsuario);	
+		Log.d("IDUSUARIO", "usuario="+idusuario.toString());
+		
+		/*****ws****/
+		
+		
+		Syncronizar sync = new Syncronizar(window);
+		List<NameValuePair> param = new ArrayList<NameValuePair>();	
+		
+		param.add(new BasicNameValuePair("idvendedor", idusuario));
+        
+        param.add(new BasicNameValuePair("dni", dni));
+        param.add(new BasicNameValuePair("nombre", nombre));
+        param.add(new BasicNameValuePair("apepaterno", paterno));
+        param.add(new BasicNameValuePair("apematerno", materno));
+        param.add(new BasicNameValuePair("telefono",  fono));
+        param.add(new BasicNameValuePair("email",  correo));
+        param.add(new BasicNameValuePair("fechanac", fecha)); // Revisar Nombre de FechaNac
+   
+        param.add(new BasicNameValuePair("ruc", ruc));
+        param.add(new BasicNameValuePair("razon_social", rsocial));
+        param.add(new BasicNameValuePair("direccion", direcc));
+        param.add(new BasicNameValuePair("latitud", latitud));
+        param.add(new BasicNameValuePair("longitud", longitud));
+		
+//		String route="dp2/itrade/ws/cobranza/get_all_products";
+		//String route="/ws/clientes/get_clients_by_idvendedor_p/";
+		String route="/ws/clientes/registrar_prospecto/";
+		
+		sync.conexion(param,route);
+		
+		try {
+			sync.getHilo().join();
+			Log.d("TAG","LLEGA PRIMERO AKI");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+}
+
 	
 }
