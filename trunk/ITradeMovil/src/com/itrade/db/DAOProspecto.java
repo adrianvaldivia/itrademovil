41cf8e56d9154cb906ad1518df90e638bc58e09c
jorgecornejo0487@gmail.com <jorgecornejo0487@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-11-15 04:52:03 +0000

    jc

--- trunk/ITradeMovil/src/com/itrade/db/DAOProspecto.java
+++ trunk/ITradeMovil/src/com/itrade/db/DAOProspecto.java
@@ -70,10 +70,8 @@ private Activity window;
 	}
 	
 
-public void registrarProspecto(String idusuario, String dni, String nombre, String paterno, String materno, String fono,
-									String correo, String fecha, String ruc, String rsocial, String direcc, String latitud,
-									String longitud) {
-		
+public int registrarProspecto(Prospecto client, long idu) {
+		int idProspecto=-1;
 		Intent i = this.window.getIntent();                
 		//idusuario=String.valueOf(idUsuario);	
 		Log.d("IDUSUARIO", "usuario="+idusuario.toString());
@@ -84,21 +82,25 @@ public void registrarProspecto(String idusuario, String dni, String nombre, Stri
 		Syncronizar sync = new Syncronizar(window);
 		List<NameValuePair> param = new ArrayList<NameValuePair>();	
 		
-		param.add(new BasicNameValuePair("idvendedor", idusuario));
+		String idu2=""+idu;
+		
+		 param.add(new BasicNameValuePair("idvendedor", idu2));
+         
+         param.add(new BasicNameValuePair("dni", client.getDNI()));
+         param.add(new BasicNameValuePair("nombre", client.getNombre()));
+         param.add(new BasicNameValuePair("apepaterno", client.getApePaterno()));
+         param.add(new BasicNameValuePair("apematerno", client.getApeMaterno()));
+         param.add(new BasicNameValuePair("telefono",  client.getTelefono()));
+         param.add(new BasicNameValuePair("email",  client.getEmail()));
+         param.add(new BasicNameValuePair("fechanac", client.getFechNac())); // Revisar Nombre de FechaNac
+    
+         param.add(new BasicNameValuePair("ruc", client.getRUC()));
+         param.add(new BasicNameValuePair("razon_social", client.getRazon_Social()));
+         param.add(new BasicNameValuePair("direccion", client.getDireccion()));
+         param.add(new BasicNameValuePair("latitud", client.getLatitud().toString() ));
+         param.add(new BasicNameValuePair("longitud", client.getLongitud().toString()));
         
-        param.add(new BasicNameValuePair("dni", dni));
-        param.add(new BasicNameValuePair("nombre", nombre));
-        param.add(new BasicNameValuePair("apepaterno", paterno));
-        param.add(new BasicNameValuePair("apematerno", materno));
-        param.add(new BasicNameValuePair("telefono",  fono));
-        param.add(new BasicNameValuePair("email",  correo));
-        param.add(new BasicNameValuePair("fechanac", fecha)); // Revisar Nombre de FechaNac
-   
-        param.add(new BasicNameValuePair("ruc", ruc));
-        param.add(new BasicNameValuePair("razon_social", rsocial));
-        param.add(new BasicNameValuePair("direccion", direcc));
-        param.add(new BasicNameValuePair("latitud", latitud));
-        param.add(new BasicNameValuePair("longitud", longitud));
+         param.add(new BasicNameValuePair("montosolicitado", ""+client.getMontoActual() ));
 		
 //		String route="dp2/itrade/ws/cobranza/get_all_products";
 		//String route="/ws/clientes/get_clients_by_idvendedor_p/";
@@ -113,6 +115,13 @@ public void registrarProspecto(String idusuario, String dni, String nombre, Stri
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	    Log.d("TAG","LLEGA SEGUNDO AKI");
+	    Gson gson = new Gson();
+							
+		Log.e("log_tag", "se cayo5" );
+		idProspecto	=	gson.fromJson(sync.getResponse(), new TypeToken<Integer>(){}.getType());	
+		
+		return idProspecto;
 
 }
 
