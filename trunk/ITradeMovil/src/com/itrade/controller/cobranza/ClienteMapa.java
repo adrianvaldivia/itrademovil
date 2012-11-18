15e86bbabc30680a1637fd3f9dea7b443c3e8210
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-11-18 07:58:47 +0000

    SyncEventos agregado y clase necesaria para el CheckIn

---
+++ trunk/ITradeMovil/src/com/itrade/controller/cobranza/ClienteMapa.java
@@ -0,0 +1,70 @@
+package com.itrade.controller.cobranza;
+
+public class ClienteMapa {
+
+	/**
+	 * @param args
+	 */
+	private Integer IdCliente;
+	private String Nombre;
+	private String ApePaterno;
+	private String ApeMaterno;
+	private Double Latitud;
+	private Double Longitud;
+	private Integer CheckIn;
+	
+	public ClienteMapa(Integer idCliente, String nombre, String apePaterno,
+			String apeMaterno, Double latitud, Double longitud, Integer checkIn) {
+		super();
+		IdCliente = idCliente;
+		Nombre = nombre;
+		ApePaterno = apePaterno;
+		ApeMaterno = apeMaterno;
+		Latitud = latitud;
+		Longitud = longitud;
+		CheckIn = checkIn;
+	}
+	
+	public Integer getIdCliente() {
+		return IdCliente;
+	}
+	public void setIdCliente(Integer idCliente) {
+		IdCliente = idCliente;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApePaterno() {
+		return ApePaterno;
+	}
+	public void setApePaterno(String apePaterno) {
+		ApePaterno = apePaterno;
+	}
+	public String getApeMaterno() {
+		return ApeMaterno;
+	}
+	public void setApeMaterno(String apeMaterno) {
+		ApeMaterno = apeMaterno;
+	}
+	public Double getLatitud() {
+		return Latitud;
+	}
+	public void setLatitud(Double latitud) {
+		Latitud = latitud;
+	}
+	public Double getLongitud() {
+		return Longitud;
+	}
+	public void setLongitud(Double longitud) {
+		Longitud = longitud;
+	}
+	public Integer getCheckIn() {
+		return CheckIn;
+	}
+	public void setCheckIn(Integer checkIn) {
+		CheckIn = checkIn;
+	}
+}
