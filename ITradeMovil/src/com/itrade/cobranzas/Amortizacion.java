fe099837cd3512777b17ba3d1af29d3c0c4bed5b
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-11-28 03:26:37 +0000

    Amortizacion linkeados

--- ITradeMovil/src/com/itrade/cobranzas/Amortizacion.java
+++ ITradeMovil/src/com/itrade/cobranzas/Amortizacion.java
@@ -9,13 +9,17 @@ import org.apache.http.message.BasicNameValuePair;
 import com.itrade.R;
 import com.itrade.controller.cobranza.SyncDeposito;
 import com.itrade.controller.cobranza.SyncDetallePedido;
+import com.itrade.controller.cobranza.SyncNotifications;
 import com.itrade.controller.cobranza.SyncPedidos;
 import com.itrade.controller.cobranza.Syncronizar;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -37,6 +41,18 @@ public class Amortizacion extends Activity {
 	private SyncPedidos syncPedido;
 	private SyncDeposito syncDeposito;
 	private Double montoMaximoDeposito;
+	//Botones
+	private ImageView btnClientes;
+	private ImageView btnMail;
+	private ImageView btnBuscar;
+	private ImageView btnDepositar;
+	private ImageView btnDirectorio;
+	private ImageView btnCalendario;
+	private ImageView btnMapaTotal;
+	//Context
+	final Context context = this;
+	//Syncronization
+	private SyncNotifications sincNotifications;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +66,127 @@ public class Amortizacion extends Activity {
         setValues();
         //txtVwMontoCobrado.setText(sync.getResponse().toString());	
        //syncPedido.closeDB();
+        /*BTN clientes*/
+		btnClientes= (ImageView)findViewById(R.id.btnListaClientes);
+		btnClientes.setOnClickListener(new OnClickListener() {			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Amortizacion.this, ClientesListTask.class); 																				
+				intent.putExtra("idempleado", idusuario);
+				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+			}
+		});
+		
+		/*BTN clientes*/
+		btnDepositar= (ImageView)findViewById(R.id.btnDepositarBanco);
+		btnDepositar.setOnClickListener(new OnClickListener() {			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Amortizacion.this, Amortizacion.class); 																				
+				intent.putExtra("idusuario", idusuario);
+				startActivity(intent);
+			}
+		});
+		/*BTN Mensaje masivo*/
+		btnMail= (ImageView)findViewById(R.id.btnMailMasivo);
+		btnMail.setOnClickListener(new OnClickListener() {			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (!sincNotifications.sendNotification(idusuario)){
+					String titulo="Notificar"; 
+					String mensaje="¿Deseas Notificar ahora a todos tus clientes ?"; 				
+					AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);		 				
+					alertDialogBuilder.setTitle(titulo);		 			
+					alertDialogBuilder
+							.setMessage(mensaje)
+							.setCancelable(true)
+							.setNegativeButton("Cancelar", null)
+							.setPositiveButton("Aceptar",new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,int id) {														
+									dialog.cancel();
+									//verificar si tiene internet o no <--------------------------									
+									if (networkAvailable()){										
+											Syncronizar sync = new Syncronizar(Amortizacion.this);
+											List<NameValuePair> param = new ArrayList<NameValuePair>();
+											param.add(new BasicNameValuePair("idcobrador", idusuario));
+											String route2="/ws/cobranza/send_notifications/";
+											sync.conexion(param,route2);
+											try {
+												sync.getHilo().join();
+											} catch (InterruptedException e) {
+												  // TODO Auto-generated catch block
+												e.printStackTrace();
+											}									
+											sincNotifications.saveNotification(idusuario);
+											Toast.makeText(Amortizacion.this, "Se notificó exitosamente a los clientes.", Toast.LENGTH_SHORT).show();
+											Intent intent = new Intent(Amortizacion.this, ClientesListTask.class); 													
+											intent.putExtra("idempleado", idusuario);
+											startActivity(intent);
+																												
+									}else{
+										Toast.makeText(Amortizacion.this, "Necesita conexión a internet para nofiticar", Toast.LENGTH_SHORT).show();
+									}																																						
+								}
+					});		
+					AlertDialog alertDialog = alertDialogBuilder.create();		 
+					alertDialog.show();
+				}else{
+					Toast.makeText(Amortizacion.this, "Usted ya envió notifaciones el dia de hoy.", Toast.LENGTH_SHORT).show();
+				}			
+			}
+		});
+		/*btn buscar clientes*/
+		btnBuscar= (ImageView)findViewById(R.id.btnBuscarCliente);
+		btnBuscar.setOnClickListener(new OnClickListener() {			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Amortizacion.this, Buscaclientes.class);		
+				intent.putExtra("idusuario", idusuario);
+				intent.putExtra("boolVer", 1);
+				startActivity(intent);
+			}
+		});
+		/**/
+		btnCalendario= (ImageView)findViewById(R.id.btnCalendario);
+		btnCalendario.setOnClickListener(new OnClickListener() {			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Amortizacion.this, Calendario.class);
+				intent.putExtra("idusuario", idusuario);				
+				startActivity(intent);
+			}
+		});	
+		btnDirectorio= (ImageView)findViewById(R.id.btnDirectorio);
+		btnDirectorio.setOnClickListener(new OnClickListener() {			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Amortizacion.this, Directorio.class);
+				intent.putExtra("idusuario", idusuario);				
+				startActivity(intent);
+			}
+		});	
+		/*btn Mapa Clientes*/
+		btnMapaTotal= (ImageView)findViewById(R.id.btnMapa);
+		btnMapaTotal.setOnClickListener(new OnClickListener() {			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (networkAvailable()){
+					Intent intent = new Intent(Amortizacion.this, MapaClientes.class);		
+					intent.putExtra("idempleado", idusuario);				
+					startActivity(intent);
+				}else{
+					Toast.makeText(Amortizacion.this, "Necesita conexion a internet para ver el mapa", Toast.LENGTH_SHORT).show();
+				}				
+			}
+		});	
+		
         
         
 	}
 	private void sqlite() {
 		// TODO Auto-generated method stub		
-		
+		sincNotifications = new SyncNotifications(Amortizacion.this);
 //		Integer numreg = syncDetalle.syncBDToSqlite(idpedido);
 //		Log.d("RESULTADOS","numeros ="+numreg.toString());	
 		
@@ -100,5 +231,31 @@ public class Amortizacion extends Activity {
 		txtVwMontoDepositado.setText(syncDeposito.totalDepositado(idusuario).toString());
 		montoMaximoDeposito=syncPedido.getMontoTotalCobrado(idusuario)-syncDeposito.totalDepositado(idusuario);	
 		montoMaximoDeposito=Math.round(montoMaximoDeposito*100.0)/100.0;
-	}	
+		btnClientes=(ImageView)findViewById(R.id.btnListaClientes);
+		btnMail=(ImageView)findViewById(R.id.btnMailMasivo);
+		btnBuscar=(ImageView)findViewById(R.id.btnBuscarCliente);
+		btnDepositar=(ImageView)findViewById(R.id.btnDepositarBanco);
+		btnDirectorio=(ImageView)findViewById(R.id.btnDirectorio);
+		btnCalendario=(ImageView)findViewById(R.id.btnCalendario);
+		btnMapaTotal=(ImageView)findViewById(R.id.btnMapa);
+		
+		
+	}
+	public boolean networkAvailable() {    	
+     	ConnectivityManager connectMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+     	if (connectMgr != null) {
+     		NetworkInfo[] netInfo = connectMgr.getAllNetworkInfo();
+     		if (netInfo != null) {
+     			for (NetworkInfo net : netInfo) {
+     				if (net.getState() == NetworkInfo.State.CONNECTED) {
+     					return true;
+     				}
+     			}
+     		}
+     	} 
+     	else {
+     		Log.d("NETWORK", "No network available");
+     	}
+     	return false;
+     }
 }
