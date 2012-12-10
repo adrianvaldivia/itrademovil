c7fdcfdf713ae9872ad87081b820645ae7e58a1e
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-12-10 00:13:17 +0000

    Eventos modificado chichan acuerdo 3 webservices

--- trunk/itrade_web/application/controllers/ws/eventos.php
+++ trunk/itrade_web/application/controllers/ws/eventos.php
@@ -14,26 +14,44 @@ class Eventos extends CI_Controller {
 		echo "Controlador Pedido";			
 	}	
 	
-	public function registrar_evento($idcreador_w='',$descripcion_w='',$fecha_w='',$horaini_w='',$horafin_w='',$lugar_w='',$invitados_w=''){	
+	public function registrar_evento($idcreador_w='',$descripcion_w='',$fecha_w='',$horaini_w='',$horafin_w='',$lugar_w=''){	
 		$idcreador=$this->input->post('idcreador');
 		$descripcion=$this->input->post('descripcion');
 		$fecha=$this->input->post('fecha');
 		$horaini=$this->input->post('horaini');
 		$horafin=$this->input->post('horafin');				
-		$lugar=$this->input->post('lugar');
-		$invitados=$this->input->post('invitados');
+		$lugar=$this->input->post('lugar');		
 		if (isset($idcreador_w)&& $idcreador_w!=""  ){
 			$idcreador=$idcreador_w;
 			$descripcion=$descripcion_w;
 			$fecha=$fecha_w;
 			$horaini=$horaini_w;
 			$horafin=$horafin_w;
-			$lugar=$lugar_w;
+			$lugar=$lugar_w;			
+		}
+		$result=$this->Evento_model->registrar_evento($idcreador,$descripcion,$fecha,$horaini,$horafin,$lugar);
+		$this->output->set_content_type('application/json')->set_output($result);					
+	}
+	
+	public function registrar_invitados($idevento_w='',$invitados_w=''){
+		$invitados=$this->input->post('invitados');
+		$idevento=$this->input->post('idevento');
+		if (isset($idevento_w)&& $idevento_w!=""  ){			
+			$idevento=$idevento_w;
 			$invitados=$invitados_w;
 		}
-		$result=$this->Evento_model->registrar_evento($idcreador,$descripcion,$fecha,$horaini,$horafin,$lugar,$invitados);
-		$this->output->set_content_type('application/json')->set_output(json_encode($result));					
+		$result=$this->Evento_model->registrar_invitados($idevento,$invitados);
+		$this->output->set_content_type('application/json')->set_output($result);
 	}
+	public function contactos_por_evento($ideventos_w=''){
+		$ideventos=$this->input->post('ideventos');
+		if (isset($ideventos_w)&& $ideventos_w!="" ){
+			$ideventos=$ideventos_w;
+		}
+		$result=$this->Evento_model->contactos_por_evento($ideventos);
+		$this->output->set_content_type('application/json')->set_output(json_encode($result));							
+	}
+	
 	public function get_eventos_idusuario_month($idusuario_w='',$fecha_w=''){
 		$idusuario=$this->input->post('idusuario');
 		$fecha=$this->input->post('fecha');	
