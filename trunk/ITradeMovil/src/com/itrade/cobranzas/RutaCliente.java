4b4bb5936e501500f4dd22f786644251c47fbea4
eder.santander.123@gmail.com <eder.santander.123@gmail.com@d35bd17e-53f3-2faf-7c51-b37bb791045c>
2012-12-11 05:26:53 +0000

    Ruta con camino incluido

--- trunk/ITradeMovil/src/com/itrade/cobranzas/RutaCliente.java
+++ trunk/ITradeMovil/src/com/itrade/cobranzas/RutaCliente.java
@@ -21,7 +21,9 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.itrade.R;
 import com.itrade.controller.cobranza.Syncronizar;
+import com.itrade.jsonParser.AsTaskCargarRuta;
 import com.itrade.model.Cliente;
+import com.itrade.pedidos.UbicacionCheckInActivity;
 
 import android.app.Activity;
 import android.content.ContentResolver;
@@ -85,7 +87,9 @@ public class RutaCliente  extends Activity implements LocationListener {
     static Context context = null;
     boolean primeraVez=false;
     boolean boolHayGPS=true;
+    boolean boolSeDibujo=false;
 
+    private GeoPoint ubikCliente;
         
     // ===========================================================
     // Constructors
@@ -95,98 +99,55 @@ public class RutaCliente  extends Activity implements LocationListener {
     public void onCreate(final Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState); 
     	setContentView(R.layout.c_ruta);        
-    	Intent i = getIntent();  			
-			
-//			idruta = bundle.getInt("idruta");
+    	Intent i = getIntent();  						
+    	setTitle("I Trade - Mi Ubicacion");
 
-			setTitle("I Trade - Mi Ubicacion");
-            //daoPara = new DAOParadero();
-			//inicio de green Dao
-	        //inicio green Dao
-			 
-			String IdCliente=(String)i.getSerializableExtra("idcliente"); 
-			  Syncronizar sync = new Syncronizar(RutaCliente.this);
-				List<NameValuePair> param = new ArrayList<NameValuePair>();								
-				param.add(new BasicNameValuePair("idcliente", IdCliente));	
+    	String IdCliente=(String)i.getSerializableExtra("idcliente"); 
+    	Syncronizar sync = new Syncronizar(RutaCliente.this);
+    	List<NameValuePair> param = new ArrayList<NameValuePair>();								
+    	param.add(new BasicNameValuePair("idcliente", IdCliente));	
 					//String route="dp2/itrade/ws/clientes/get_clientes_by_vendedor/";
-					String route="ws/clientes/get_cliente_by_id";
-				    sync.conexion(param,route);
-				    try {
-						sync.getHilo().join();			
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}	    	    
-					Log.d("ClienteeeEder", "hhahahhahahhah");
-				    Gson gson = new Gson();
-										
-				    listaCliente	=	gson.fromJson(sync.getResponse(), new TypeToken<List<Cliente>>(){}.getType());		
-				
-	        	        
-	        //fin green dao
-	       // listaCliente=
-//            listaCliente= daoPara.getAllParaderos(idruta);//idRuta
-            listaGeoPoint=this.Convierte(listaCliente);
-
-            mResourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
-
-            final RelativeLayout rl = new RelativeLayout(this);
-
-            this.mOsmv = new MapView(this, 256);
-            rl.addView(this.mOsmv, new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT,
-                            LayoutParams.FILL_PARENT));
-
-            /* Itemized Overlay */
-            {
-                    /* Create a static ItemizedOverlay showing a some Markers on some cities. */
-
-
-                    /* OnTapListener for the Markers, shows a simple Toast. */
-                    this.mMyLocationOverlay = new ItemizedIconOverlay<OverlayItem>(items,
-                                    new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+    	String route="ws/clientes/get_cliente_by_id";
+    	sync.conexion(param,route);
+    	try {
+    		sync.getHilo().join();			
+    	} catch (InterruptedException e) {
+    		//TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}	    	    			
+    	Gson gson = new Gson();									
+    	listaCliente = gson.fromJson(sync.getResponse(), new TypeToken<List<Cliente>>(){}.getType());		
+    	listaGeoPoint=this.Convierte(listaCliente);
+    	mResourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
+    	final RelativeLayout rl = new RelativeLayout(this);
+    	this.mOsmv = new MapView(this, 256);
+        rl.addView(this.mOsmv, new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT));           
+        {
+        	this.mMyLocationOverlay = new ItemizedIconOverlay<OverlayItem>(items,new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
                               				//single tap
-                                            public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
-                                                    Toast.makeText(
-                                                                    RutaCliente.this,
-                                                                    item.mDescription+"-"+item.getTitle() , Toast.LENGTH_LONG).show();
-                                                    return true; // We 'handled' this event.
-                                            }
+	        	public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
+	        		Toast.makeText(RutaCliente.this,item.mDescription+"-"+item.getTitle() , Toast.LENGTH_LONG).show();
+	        		return true; // We 'handled' this event.
+	        	}
+	
+	                                            //long pressed
+	        	public boolean onItemLongPress(final int index, final OverlayItem item) {
+	                return false;
+	        	}
+        	}, mResourceProxy);
+        	this.mOsmv.getOverlays().add(this.mMyLocationOverlay);
+        }
 
-                                            //long pressed
-                                            public boolean onItemLongPress(final int index, final OverlayItem item) {
-                                                   
-                                                    return false;
-                                            }
-                                    }, mResourceProxy);
-                    this.mOsmv.getOverlays().add(this.mMyLocationOverlay);
-            }
-
-            this.setContentView(rl);
-            mOsmv.setBuiltInZoomControls(true);
-            mOsmv.setMultiTouchControls(true);
-  
-            mapController = mOsmv.getController();
-            //mapController.setZoom(5);//-12.071208,-77.077569
+        this.setContentView(rl);
+        mOsmv.setBuiltInZoomControls(true);
+        mOsmv.setMultiTouchControls(true);
+        mapController = mOsmv.getController();          
           			
-            GeoPoint gPt0 = new GeoPoint(-12071208,-77077569);//pucp  
-//            GeoPoint gPt0 = new GeoPoint(49406100,8715140);//Alemania  
-         //   mapController.setCenter(gPt0);
-            mapController.setZoom(13);
-            //////////////////////////////////////////////////////LAYER DE RUTA
-            myPath = new PathOverlay(Color.BLACK, this);
-            cargarGeoPoints();      
-            if (this.listaGeoPoint!=null)
-            	mapController.setCenter(gPt0);
-            mOsmv.getOverlays().add(myPath);
-          ////////////////////////////////////////////////////////LAYER DE POSICION ACTUAL
-            this.posicionActualOverlay = new SimpleLocationOverlay(this);
-            
-            mOsmv.getOverlays().add(posicionActualOverlay);
-
+        GeoPoint gPt0 = new GeoPoint(-12071208,-77077569);//pucp  
+        mapController.setZoom(13);
+        this.posicionActualOverlay = new SimpleLocationOverlay(this);            
+        mOsmv.getOverlays().add(posicionActualOverlay);
     }
-
-
-
 
     @Override
     public boolean onMenuItemSelected(final int featureId, final MenuItem item) {
@@ -197,17 +158,6 @@ public class RutaCliente  extends Activity implements LocationListener {
     // ===========================================================
     // Methods
     // ===========================================================
-    private void cargarGeoPoints() {
-		// TODO Auto-generated method stub
-    	for(int i=0;i<this.listaGeoPoint.size();i++){
-    		myPath.addPoint(this.listaGeoPoint.get(i));
-    	}
-    	if (listaGeoPoint.isEmpty()){
-    		listaGeoPoint=null;
-    	}
-    		
-		
-	}
 	private List<GeoPoint> Convierte(List<Cliente> lis) {
 		List<GeoPoint> lista=new ArrayList<GeoPoint>();;
 		int i;
@@ -217,23 +167,14 @@ public class RutaCliente  extends Activity implements LocationListener {
 			posxint=(int)posxdouble;
 			posyint=(int)posydouble;
 			GeoPoint aux = new GeoPoint(posxint,posyint);
+			ubikCliente= aux;
 			lista.add(aux);
 		}
 		if (!lis.isEmpty()){
-			//inicio cambios icono por defecto
-//			items.add(new OverlayItem(lis.get(0).getNombre(), "SampleDescription1", lista.get(0)));
-//	        //icono customizado        
-//	        OverlayItem olItem = new OverlayItem(lis.get(i-1).getNombre(), "SampleDescription", lista.get(i-1));
-//	        Drawable newMarker = this.getResources().getDrawable(R.drawable.marker);
-//	        olItem.setMarker(newMarker);
-//	        items.add(olItem);
-	        //fin cambios
 			for(i=0;i<lis.size();i++){
 				items.add(new OverlayItem(lis.get(i).getNombre()+" "+lis.get(i).getApePaterno(), "Cliente", lista.get(i)));
 			}  
 		}
-
-
 		return lista;
 	}
     // ===========================================================
@@ -265,17 +206,16 @@ public class RutaCliente  extends Activity implements LocationListener {
             if(loc != null) {
                 double latitude = loc.getLatitude();
                 double longitude = loc.getLongitude();
-                if (latitude!=0){
-                	GeoPoint p = new GeoPoint((int) (latitude * 1000000), (int) (longitude * 1000000));
-                	
-                	posicionActualOverlay.setLocation(p);
-                	cargarGeoPoints();
-                	myPath.addPoint(posicionActualOverlay.getMyLocation());
+                if (latitude!=0 && !boolSeDibujo){
+                	GeoPoint p = new GeoPoint((int) (latitude * 1000000), (int) (longitude * 1000000));                	
+                	posicionActualOverlay.setLocation(p);                	                                	               
                 	if(primeraVez){
                 		mapController.setCenter(p);
                 		primeraVez=false;
-                	}        			
-                    //Toast.makeText(MiUbicacionImplActivity.this,"Longitude is  "+longitude + "   Latitude is   "+latitude, Toast.LENGTH_LONG).show();
+                	}
+                	//Pintar ruta
+                	dibujaRuta(p,ubikCliente);
+                    //Toast.makeText(RutaCliente.this,"Longitude is  "+longitude + "   Latitude is   "+latitude, Toast.LENGTH_LONG).show();
                 }                
             }
             else
@@ -284,7 +224,15 @@ public class RutaCliente  extends Activity implements LocationListener {
         else
         	Toast.makeText(RutaCliente.this,"Encienda el GPS, y salga fuera del edificio por favor.", Toast.LENGTH_LONG).show();
 	}
-
+	
+	private void dibujaRuta(GeoPoint geoPointOrigen,GeoPoint geoPointDestino) {
+		AsTaskCargarRuta _connectAsyncTask = new AsTaskCargarRuta(RutaCliente.this,mOsmv,
+        		Color.parseColor(getResources().getString(R.color.Aqua)),
+        		geoPointOrigen, geoPointDestino,this.mOsmv.getOverlays().size());        
+        _connectAsyncTask.execute();
+        boolSeDibujo=true;            
+	}
+	
 	 private Boolean displayGpsStatus() {  
 		  ContentResolver contentResolver = getBaseContext()  
 		  .getContentResolver();  
@@ -299,10 +247,6 @@ public class RutaCliente  extends Activity implements LocationListener {
 		  }  
 	} 
 
-
-	 
-
-	 
 	@Override
 	protected void onDestroy() {
 		//cursor.close();
@@ -355,11 +299,7 @@ public class RutaCliente  extends Activity implements LocationListener {
 			double lati = loc.getLatitude();
 			double longi = loc.getLongitude();
 	        GeoPoint p = new GeoPoint((int) (lati * 1000000), (int) (longi * 1000000));
-	//       myPath.getPaint().
 	        posicionActualOverlay.setLocation(p);
-	      myPath.clearPath();
-	      myPath.addPoint(posicionActualOverlay.getMyLocation());
-	    //  myPath.addPoint()
 	    	if (primeraVez){
 	    		mapController.setCenter(p);
 	    		primeraVez=false;
@@ -367,7 +307,6 @@ public class RutaCliente  extends Activity implements LocationListener {
 			Log.e("cambio pos","lat:" +lati+" "+ longi);
 		}				
 	}
-
 
 
 	public void onProviderDisabled(String arg0) {
